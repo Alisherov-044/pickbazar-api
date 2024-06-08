09c4b1df23b1615a8a6589cb01b15e9ec90bd1ec
Sadulla Alisherov <alisherov044@gmail.com>
2024-06-08 16:32:29 +0500

    fix: delete

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database();
+const db = new sqlite3.Database("db.sqlite");
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS categories (
