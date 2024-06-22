a65d3e180300a802f5763b1f727b3e3aff16ea0b
alisherov-cs <alisherov.cs@gmail.com>
2024-06-22 18:59:20 +0500

    feat: cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+app.use(
+    cors({
+        origin: ["*", "http://localhost:5173", "http://localhost:5174"],
+        methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    })
+);
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use("/api", routes);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
